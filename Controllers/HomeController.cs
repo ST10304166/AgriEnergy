57087b73fc56374d9f71ef7df39f809289a614db
Steven Prince Bomela <ST10304166@vcconnect.edu.za>
2025-05-12 23:19:39 +0200

    added register employee

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 using AgriEnergy.Models;
+using AgriEnergy.Utilities;
+using AgriEnergy.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace AgriEnergy.Controllers
@@ -7,10 +10,31 @@ namespace AgriEnergy.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RegisterEmployee(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", employee);
+            }
+
+            employee.Password = PasswordHasher.HashPassword(employee.Password);
+            employee.Role = "Employee";
+
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            TempData["SuccessMessage"] = "Employee registered successfully!";
+            return RedirectToAction("Index");
         }
 
         public IActionResult Index()
